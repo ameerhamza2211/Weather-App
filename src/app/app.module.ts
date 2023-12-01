c67914b1ecd170eaed50df9131fab6314c6d7a77
Ameer Hamza <ameer.hamza928942@gmail.com>
2023-12-02 01:20:29 +0500

    Working Complete copied

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { WeatherReportComponent } from './weather-report/weather-report/weather-report.component';
+import { Routes } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: WeatherReportComponent
+  },
+  {
+    path: ':locationName',
+    component: WeatherReportComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    WeatherReportComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    RouterModule.forRoot(routes),
+    MatProgressBarModule,
+    HttpClientModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subject, Observable } from 'rxjs';
+import { takeUntil, map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
+export class MainComponent implements OnInit, OnDestroy {
 
+  private unsubscribe$ = new Subject<void>();
+
+  countries = [
+      {
+        name: 'Pakistan',
+        cities: ['Lahore', 'Karachi', 'Islamabad'],
+      },
+      {
+        name: 'United States',
+        cities: ['New York', 'Chicago', 'Washington']
+      },
+      {
+        name: 'Australia',
+        cities: ['Sydney', 'Adelaide', 'Melbourne']
+      }
+  ]
+
+  countryControl!: FormControl;
+  cityControl!: FormControl;
+  cities$!: Observable<string>;
+
+  constructor(private router: Router){}
+
+  ngOnInit() {
+    this.cityControl = new FormControl('');
+    this.cityControl.valueChanges
+      .subscribe(value => {
+        this.router.navigate([value]);
+      });
+
+    this.countryControl = new FormControl('');
+
+    this.cities$ = this.countryControl.valueChanges.pipe(
+      map(country => country.cities)
+    );
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 }
